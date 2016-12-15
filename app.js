a2754b14fa3faecf702527eb459e562d56004d43
Jonathan Darling <jmdarling@me.com>
2016-12-15 14:58:58 -0600

    Enabling clustering.

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const bodyParser = require('body-parser')
+const cluster = require('cluster')
 const express = require('express')
 const redis = require('redis')
 
 const config = require('./config')
 const Controller = require('./controller')
 
-// Express.
-const app = express()
-app.use(bodyParser.json())
-app.listen(config.port, () => {
-  console.log(`Just Log Me Baby listening on port ${config.port}\n\n`)
-})
+if (cluster.isMaster) {
+  for (let i = 0; i < config.instances; i++) {
+    cluster.fork()
+  }
+} else {
+  // Express.
+  const app = express()
+  app.use(bodyParser.json())
+  app.listen(config.port, () => {
+    console.log(`Just Log Me Baby listening on port ${config.port}\n\n`)
+  })
 
-// Redis.
-const redisClient = redis.createClient(config.redisUrl)
-redisClient.on('connect', () => {
-  console.log(`Connected to redis instance at ${config.redisUrl}\n\n`)
-  const controller = new Controller(app, redisClient)
-  controller.registerRoutes()
-})
+  // Redis.
+  const redisClient = redis.createClient(config.redisUrl)
+  redisClient.on('connect', () => {
+    console.log(`Connected to redis instance at ${config.redisUrl}\n\n`)
+    const controller = new Controller(app, redisClient)
+    controller.registerRoutes()
+  })
+}
--- config.js
+++ config.js
@@ -2,5 +2,6 @@ require('dotenv').config({ silent: true })
 
 module.exports = {
   port: process.env.PORT || 8080,
+  instances: process.env.JUST_LOG_ME_BABY_INSTANCES || 1,
   redisUrl: process.env.REDIS_URL || '//localhost:6379'
 }
